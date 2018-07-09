9c76aa318a8f4faad029bfb72bb81f0f091752c7
Dasha Osipova <daria.osipova@nure.ua>
2018-07-09 17:59:05 +0300

    Add project files.

---
+++ GeometricFigure_les6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometricFigure_les6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
